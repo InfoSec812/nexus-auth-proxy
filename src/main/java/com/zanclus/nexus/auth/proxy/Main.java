605b0c678d59b0ae0f4a0ee8da0cf79a285b8dec
Deven Phillips <deven.phillips@gmail.com>
2015-02-28 00:31:02 -0500

    Debugged and tested configuration parsing

--- src/main/java/com/zanclus/nexus/auth/proxy/Main.java
+++ src/main/java/com/zanclus/nexus/auth/proxy/Main.java
@@ -1,32 +1,81 @@
 package com.zanclus.nexus.auth.proxy;
 
+import com.beust.jcommander.JCommander;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.zanclus.nexus.auth.proxy.config.Config;
+import java.lang.reflect.Field;
+import java.nio.file.Files;
+import lombok.extern.slf4j.Slf4j;
+
 /*
  * #%L
  * nexus-auth-proxy
  * %%
  * Copyright (C) 2015 Zanclus Consulting
  * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  * 
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
 
 /**
- *
- * @author dphillips
+ * Entry-point for Nexus Authentication Proxy server.
+ * @author <a href="https://github.com/InfoSec812/">Deven Phillips</a>
  */
-public interface Main {
+@Slf4j
+public class Main {
 
-    public static void main(String[] args) {
-        
+    /**
+     * Given a source and destination instance of {@link Config}, copy the non-null values from source into destination
+     * @param src The instance of {@link Config} to copy non-null values from
+     * @param dst The instance of {@link Config} to copy non-null values to
+     * @throws Exception If there is a problem doing the reflection work
+     */
+    private static void mergeConfigs(Config src, Config dst) throws Exception {
+        for (Field field: src.getClass().getDeclaredFields()) {
+            field.setAccessible(true);
+            if (field.get(src)!=null) {
+                field.set(dst, field.get(src));
+            }
+            field.setAccessible(false);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Gson gson = (new GsonBuilder())
+                            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                            .setPrettyPrinting()
+                            .excludeFieldsWithoutExposeAnnotation()
+                            .create();
+        Config params = new Config();
+        JCommander commander = new JCommander(params);
+        commander.setAcceptUnknownOptions(true);
+        commander.setProgramName("nexus-auth-proxy");
+        commander.parse(args);
+        if (params.help()) {
+            commander.usage();
+            System.exit(0);
+        }
+        LOG.debug("Parsed command-line args: \n\n"+gson.toJson(params)+"\n\n");
+        if (params.configFile().isFile() && params.configFile().canRead()) {
+            Config fileCfg = gson.fromJson(new String(Files.readAllBytes(params.configFile().toPath())), Config.class);
+            mergeConfigs(params, fileCfg);
+            params = fileCfg;
+        }
+        params.fillDefaults();
+        LOG.debug("Loaded Config:\n\n"+gson.toJson(params)+"\n\n");
     }
 }
--- src/main/java/com/zanclus/nexus/auth/proxy/config/Config.java
+++ src/main/java/com/zanclus/nexus/auth/proxy/config/Config.java
@@ -6,46 +6,79 @@ package com.zanclus.nexus.auth.proxy.config;
  * %%
  * Copyright (C) 2015 Zanclus Consulting
  * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  * 
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
 
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
+import com.google.gson.annotations.Expose;
+import java.io.File;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- *
- * @author dphillips
+ * Bean for storing configuration options. Also used by JCommander to parse command-line arguments
+ * @author <a href="https://github.com/InfoSec812/">Deven Phillips</a>
  */
 @Data
 @NoArgsConstructor
 @Parameters(separators = " =")
 public class Config {
 
-    @Parameter(description = "The hostname or IP address on which the proxy to listen for incomming requests.", names = {"--proxy-host"})
-    private String proxyInterface;
+    public void fillDefaults() {
+        if (this.proxyHost==null) {
+            this.proxyHost = "127.0.0.1";
+        }
+        if (this.proxyPort==null) {
+            this.proxyPort = 8080;
+        }
+        if (this.targetHost==null) {
+            this.targetHost = "127.0.0.1";
+        }
+        if (this.targetPort==null) {
+            this.targetPort = 8081;
+        }
+        if (this.rutHeader==null) {
+            this.rutHeader = "REMOTE_USER";
+        }
+    }
 
-    @Parameter(description = "Port on which the proxy will listen for incoming requests.", names = {"--proxy-port"})
+    @Parameter(description = "This help message", names = {"-h", "--help"}, help = true)
+    private boolean help = false;
+
+    @Parameter(description = "The hostname or IP address on which the proxy to listen for incomming requests.", names = {"-l", "--proxy-host"})
+    @Expose
+    private String proxyHost;
+
+    @Parameter(description = "Port on which the proxy will listen for incoming requests.", names = {"-p", "--proxy-port"})
+    @Expose
     private Integer proxyPort;
 
-    @Parameter(description = "The hostname or IP address of the application to be proxied.", names = {"--target-host"})
-    private String target;
+    @Parameter(description = "The hostname or IP address of the application to be proxied.", names = {"-t", "--target-host"})
+    @Expose
+    private String targetHost;
     
-    @Parameter(description = "Port of the application to be proxied.", names = {"--target-port"})
+    @Parameter(description = "Port of the application to be proxied.", names = {"-r", "--target-port"})
+    @Expose
     private Integer targetPort;
 
-    @Parameter(description = "The name of the header which should be set on proxied requests.", names = {"--rut-header"})
-    private String rutHeader = "REMOTE_USER";
+    @Parameter(description = "The name of the header which should be set on proxied requests.", names = {"-m", "--rut-header"})
+    @Expose
+    private String rutHeader;
+
+    @Parameter(description = "The path and filename from which to read the configuration.", names = {"-c", "--config"})
+    private File configFile = new File("/etc/nexus-auth-proxy/config.json");
 }
